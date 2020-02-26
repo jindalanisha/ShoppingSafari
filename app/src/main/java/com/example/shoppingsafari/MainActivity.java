b8a3804efee63d91d0c4b300175cfd94c2be5316
anisha <anishajindal143@gmail.com>
2020-02-27 01:01:30 +0530

    Home Page (displaying categories)

--- app/src/main/java/com/example/shoppingsafari/MainActivity.java
+++ app/src/main/java/com/example/shoppingsafari/MainActivity.java
@@ -1,19 +1,47 @@
 package com.example.shoppingsafari;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+   Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Log.e("Hey","Bye");
+        b=(Button)findViewById(R.id.login);
     }
+    public void gotohomepage(View view)
+    {
+        Intent I=new Intent(this,homepage.class);
+        startActivity(I);
+    }
+
+
+
 }
+
+
+
+
 
